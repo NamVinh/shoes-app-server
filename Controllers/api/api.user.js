c0f0034da2a26a7b75310c4aa5ae141e10272173
Nam Vinh <dnamvinh@gmail.com>
2020-12-21 04:52:01 +0700

    fix signup user

--- Controllers/api/api.user.js
+++ Controllers/api/api.user.js
@@ -37,15 +37,27 @@ const getUserIsLoggedIn = async (req, res) => {
 };
 const createAccount = async (req, res) => {
   try {
-    
-    let userData = await User.create({
-      ...req.body,
-      password: createHash(req.body.password),
-    });
-    return res.status(200).json({ status: 200, msg: 'Tạo tài khoản thành công',
-    version: 'v1.0', data: userData});
+    let user = await User.find({ gmail });
+    if(user) {return res.status(400).send({
+      api_status: 400,
+      api_message: 'Email đã tồn tại',
+      api_version: 'v1.0',})} 
+      else {  
+        let userData = await User.create({
+        ...req.body,
+        password: createHash(req.body.password),
+      });
+      return res.status(200).send({
+        api_status: 200,
+        api_message: 'Tạo tài khoản thành công',
+        api_version: 'v1.0',}).json({status: true, data: userData})
+    }  
   } catch (error) {
-    return res.status(500).json({ status: 500, msg: error.message });
+    send({
+      api_status: 500,
+      api_message: 'Thất bại err',
+      api_version: 'v1.0',
+    }).json({ status: false, msg: error.message });
   }
 };
 const updateAccount = async (req, res) => {
