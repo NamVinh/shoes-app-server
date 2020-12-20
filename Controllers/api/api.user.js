c568c3b1e23dcaa39b6cbf8b741d0bc254fb0690
Nam Vinh <dnamvinh@gmail.com>
2020-12-21 05:33:17 +0700

    fix signup user second

--- Controllers/api/api.user.js
+++ Controllers/api/api.user.js
@@ -36,16 +36,57 @@ const getUserIsLoggedIn = async (req, res) => {
   }
 };
 const createAccount = async (req, res) => {
-  try {
+  // try {
     
-    let userData = await User.create({
-      ...req.body,
-      password: createHash(req.body.password),
-    });
-    return res.status(200).json({ status: true, data: userData});
-  } catch (error) {
-    return res.status(200).json({ status: false, msg: error.message });
-  }
+  //   let userData = await User.create({
+  //     ...req.body,
+  //     password: createHash(req.body.password),
+  //   });
+  //   return res.status(200).json({ status: true, data: userData});
+  // } catch (error) {
+  //   return res.status(200).json({ status: false, msg: error.message });
+  // }
+  var gmail = req.body.gmail ? {gmail: req.body.gmail} : {};
+
+  const new_user = new User({
+    avatar: req.body.avatar,
+    name: req.body.name,
+    password: req.body.password,
+    phone: req.body.phone,
+    gmail: req.body.gmail,
+  });
+
+  User.find(gmail).then((data) => {
+    console.log(data + 'jdfsjdfj');
+    if (data.length !== 0) {
+      return res.status(400).send({
+        api_status: 400,
+        api_message: 'Email đã tồn tại',
+        api_version: 'v1.0',
+      });
+    } else {
+      new_user
+        .save(new_user)
+        .then((data) => {
+          if (data) {
+            res.status(200).send({
+              api_status: 200,
+              api_message: 'Tạo tài khoản thành công',
+              api_version: 'v1.0',
+              data: data,
+            });
+          }
+        })
+        .catch((err) => {
+          res.status(500).send({
+            api_status: 500,
+            api_message: 'Thất bại err',
+            api_version: 'v1.0',
+          });
+          console.log(err);
+        });
+    }
+  });
 };
 const updateAccount = async (req, res) => {
   try {
